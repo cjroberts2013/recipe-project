52e01f1586ca26dcb1774f3259a0929548c9809e
CJ Roberts <cjroberts2013@gmail.com>
2020-06-17 00:53:49 -0500

    Recipe view is working and all data is being displayed. Need to go through and remove useless functionality and work in the display next

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /* Global State 
@@ -57,6 +58,8 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare UI for changes
+        recipeView.clearRecipe()
+        renderLoader(elements.recipe)
 
         // Create new recipe object
         state.recipe = new Recipe(id);
@@ -64,6 +67,7 @@ const controlRecipe = async () => {
         try {
             // Get recipe data
             await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
 
             // Calc servings and time 
             // POSSIBLY REMOVE THIS
@@ -71,7 +75,9 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             // Render recipe
-            console.log(state.recipe)
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
         } catch (error) {
             console.log(error)
         }
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,9 +1,13 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
-    searchResList: document.querySelector('.results__list'),
     searchRes: document.querySelector('.results'),
-    searchResPages: document.querySelector('.results__pages')
+    searchResList: document.querySelector('.results__list'),
+    searchResPages: document.querySelector('.results__pages'),
+    recipe: document.querySelector('.recipe'),
+    shopping: document.querySelector('.shopping__list'),
+    likesMenu: document.querySelector('.likes__field'),
+    likesList: document.querySelector('.likes__list')
 };
 
 export const elementStrings = {
@@ -12,7 +16,7 @@ export const elementStrings = {
 
 export const renderLoader = parent => {
     const loader = `
-        <div class='${elementStrings.loader}'>
+        <div class="${elementStrings.loader}">
             <svg>
                 <use href="img/icons.svg#icon-cw"></use>
             </svg>
@@ -23,5 +27,5 @@ export const renderLoader = parent => {
 
 export const clearLoader = () => {
     const loader = document.querySelector(`.${elementStrings.loader}`);
-    if (loader) loader.parentElement.removeChild(loader)
-}
+    if (loader) loader.parentElement.removeChild(loader);
+};
